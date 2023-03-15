418c7b1fd8e5b977a273aa8742e0e63d70849250
Václav Pavlín <vasek@blockczech.io>
2023-03-15 13:43:02 +0100

    Add PORT env

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v5"
 )
@@ -12,4 +14,6 @@ func main() {
 	e.GET("/health", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, map[string]string{"status": "ok"})
 	})
+
+	e.Start(fmt.Sprintf("0.0.0.0:%s", os.Getenv("PORT")))
 }
